df509d44d512390434a7c63dc5710fb5af42b96f
Muchammad Rafif Azis Syahlevi <arslag0258@gmail.com>
2025-06-30 23:08:03 +0700

    Add authentication, authorization, validation, and sanitization

---
+++ server/middleware/auth.js
@@ -0,0 +1,21 @@
+function ensureAuthenticated(req, res, next){
+  if(req.session && req.session.user){
+    return next();
+  }else{
+    return res.status(401).json({message: "Login dulu ya"});
+  }
+}
+
+function ensureAdmin(req, res, next){
+  if(req.session?.user?.isAdmin === 1){
+    return next();
+  }
+  return res.status(403).json({message: "Akses hanya admin"});
+}
+
+module.exports={
+  ensureAuthenticated,
+  ensureAdmin,
+};
+
+
